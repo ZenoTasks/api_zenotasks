101d7212d929227ace2ed2531f751e53101274fd
fungame2270 <tomasvictal@gmail.com>
2024-10-30 15:08:21 +0000

    added enpoints for user and tasks

--- router.py
+++ router.py
@@ -1,12 +1,23 @@
 from fastapi import APIRouter, Depends
 from auth import get_current_user
+from crud import getTasks,createTask,getTask,getUser,createUser
+from models import Task,User
+from database import get_session
+from sqlmodel import Session
 
 tasksRouter = APIRouter(
     prefix="/api",
     tags=["tasks"],
-    dependencies=[Depends(get_current_user)],
 )
 
-@tasksRouter.get("/user")
-def get_user(user: dict = Depends(get_current_user)):
-    return {"message": "Protected tasks endpoint", "user": user}
+@tasksRouter.get("/me")
+def get_Me(user: User = Depends(get_current_user)) -> User:
+    return user
+
+@tasksRouter.get("/tasks")
+def get_Tasks(limit: int = 10, offset: int = 0, session: Session = Depends(get_session),user: User = Depends(get_current_user)) -> list[Task]:
+    return getTasks(limit=limit,offset=offset,session=session,user=user)
+
+@tasksRouter.post("/tasks")
+def create_Task(task: Task, session: Session = Depends(get_session),user: User = Depends(get_current_user)) -> Task:
+    return createTask(task,session=session,user=user)
