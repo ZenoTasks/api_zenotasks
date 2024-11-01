28bd237cae7c8fa33e6f797aaa7bcf379efbfa89
fungame2270 <tomasvictal@gmail.com>
2024-11-01 15:56:31 +0000

    added done field to task

--- models/task.py
+++ models/task.py
@@ -5,5 +5,6 @@ class Task(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     title: str
     description: str
+    done: bool = False
     user_id: int | None = Field(foreign_key="user.id")
     user: User | None = Relationship(back_populates="tasks")
