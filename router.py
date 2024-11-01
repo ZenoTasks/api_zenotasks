ee8405de841adfdb7d9871efd53ee202222c3eda
fungame2270 <tomasvictal@gmail.com>
2024-11-01 15:00:56 +0000

    added delete task endpoint

--- router.py
+++ router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from auth import get_current_user
-from crud import getTasks,createTask,getTask,getUser,createUser
+from crud import getTasks,createTask,deleteTask
 from models import Task,User
 from database import get_session
 from sqlmodel import Session
@@ -21,3 +21,7 @@ def get_Tasks(limit: int = 10, offset: int = 0, session: Session = Depends(get_s
 @tasksRouter.post("/tasks")
 def create_Task(task: Task, session: Session = Depends(get_session),user: User = Depends(get_current_user)) -> Task:
     return createTask(task,session=session,user=user)
+
+@tasksRouter.delete("/tasks/{task_id}")
+def delete_Task(task_id: int, session: Session = Depends(get_session),user: User = Depends(get_current_user)) -> Task:
+    return deleteTask(id=task_id,session=session,user=user)
