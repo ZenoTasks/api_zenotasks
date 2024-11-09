ca4ac81ec76446e94c42402ac6470303c1b42c3e
fungame2270 <tomasvictal@gmail.com>
2024-11-09 15:16:00 +0000

    added env load

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel, Session
+from settings import settings
 
 
 
-DATABASE_URL = f"postgresql://postgres:postgres@db:5432/zenotasks"
+DATABASE_URL = f"postgresql://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
 
 engine = create_engine(DATABASE_URL,echo=True)
 
---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+
+class Settings:
+    def __init__(self):
+        self.db_user = os.getenv("DB_USER")
+        self.db_password = os.getenv("DB_PASSWORD")
+        self.db_host = os.getenv("DB_HOST")
+        self.db_port = os.getenv("DB_PORT")
+        self.db_name = os.getenv("DB_NAME")
+
+settings = Settings()
